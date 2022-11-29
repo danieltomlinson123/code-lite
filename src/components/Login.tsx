e9ea3f56f5f994a705f31ebf592fc6267dee5675
donyol <danieltomlinson123@gmail.com>
2022-11-29 12:05:29 +0100

    vinci color set, login components resized and arranged, image max width removed to fill grid cell

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -11,11 +11,9 @@ const Login: FC = () => {
   return (
     <div className="grid grid-cols-2 items-center h-full w-full">
 
-    
-      
-        <div className="justify-self-center max-w-md">
-        <div className="flex align-start">
-          <h1 className="text-4xl text-black-600">VINCI</h1><span className="text-xs text-violet-600 mt-1 ml-0.5">BETA</span>
+        <div className="justify-self-center w-full max-w-sm flex flex-col">
+        <div className="flex align-start justify-center">
+          <h1 className="text-4xl text-black-600 font-semibold">VINCI</h1><span className="text-xs text-vinci-light mt-1 ml-0.5 font-semibold">BETA</span>
         </div>
         <form
           className="flex flex-col mt-12 space-y-6 "
@@ -28,26 +26,26 @@ const Login: FC = () => {
         >
           <input
             type="email"
-            className="mt-2 text-black rounded relative border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-400 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 text-lg"
+            className="text-black rounded-lg relative border border-stone-300 px-5 py-4 text-stone-900 placeholder-stone-400 focus:z-10 focus:border-vinci focus:outline-none focus:ring-vinci text-lg"
             onChange={(e) => {
               setEmail(e.target.value);
             }}
             value={email}
             placeholder="Enter your email"
           />
-          {/* <input id="email-address" name="email" type="email" className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address"></input> */}
+          {/* <input id="email-address" name="email" type="email" className="relative block w-full appearance-none rounded-none rounded-t-md border border-stone-300 px-3 py-2 text-stone-900 placeholder-stone-500 focus:z-10 focus:border-vinci focus:outline-none focus:ring-vinci sm:text-sm" placeholder="Email address"></input> */}
           <input
             type="password"
-            className="mt-2 text-black relative rounded border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-400 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 text-lg"
+            className="mt-4 text-black relative rounded-lg border border-stone-300 px-5 py-4 text-stone-900 placeholder-stone-400 focus:z-10 focus:border-vinci focus:outline-none focus:ring-vinci text-lg"
             onChange={(e) => {
               setPassword(e.target.value);
             }}
             value={password}
             placeholder="Enter your password"
           />
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between mt-4">
           <div className="flex items-center">
-            <input id="remember-me" name="remember-me" type="checkbox" className="h-3 w-3 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
+            <input id="remember-me" name="remember-me" type="checkbox" className="h-3 w-3 rounded-lg py-4 border-stone-300 text-vinci focus:ring-vinci-light" />
             <label htmlFor="remember-me" className="ml-1 block text-sm">Remember me</label>
           </div>
 
@@ -55,21 +53,21 @@ const Login: FC = () => {
             <a href="#" className="text-sm hover:text-indigo-500">Forgot password?</a>
           </div>
         </div>
-          <input className="mt-2 hover:bg-white/20 cursor-pointer rounded bg-violet-600 text-lg py-2 text-white" type="submit" value="Continue"/>
+          <input className="mt-4 hover:bg-white/20 cursor-pointer rounded-lg bg-vinci hover:bg-vinci-hover text-lg py-4 text-white font-semibold" type="submit" value="Continue"/>
         </form>
-        <div className="h-px bg-black"> </div>
-        <div className="flex items-center justify-center">
+        <div className="h-px bg-stone-300 mt-8"> </div>
+        <div className="flex items-center justify-center mt-4">
           <div className="flex items-center">
-            <label htmlFor="remember-me" className="ml-2 block text-lg text-gray-900">Don't have an account?</label>
+            <div className="ml-2 block text-lg text-stone-400">Don't have an account?</div>
           </div>
 
           <div className="text-sm">
-            <a href="#" className="text-lg text-indigo-600 hover:text-indigo-500">Sign in</a>
+            <a href="#" className="text-lg text-violet-600 hover:text-violet-500 ml-1">Sign up</a>
           </div>
         </div>
         </div>
         <div className="bg-white justify-self-center h-full w-full">
-        <img src="../../image_geometric.png" alt="3D geometric shapes" />
+        <img src="../../image_geometric.png" alt="3D geometric shapes" className="max-w-none"/>
       </div>
         </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        vinci: {
+          light: '#bba3f5',
+          DEFAULT: '#6d3af9',
+          hover: '#7b4df9'
+          // dark: '#6d3af9'
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
