cd2612b13e4c76838a0df1a2aa574f50181f2a62
donyol <danieltomlinson123@gmail.com>
2022-11-30 19:03:43 +0100

    update padding on input fields and check box size

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -34,7 +34,7 @@ const Login: FC = () => {
         >
           <input
             type="email"
-            className="text-black rounded-lg relative border border-stone-300 px-5 py-4 text-stone-900 placeholder-stone-400 focus:z-10 focus:border-vinci focus:outline-none focus:ring-vinci text-lg"
+            className="text-black rounded-lg relative border border-stone-300 px-5 pt-3 pb-2 text-stone-900 placeholder-stone-300 focus:z-10 focus:border-vinci focus:outline-none focus:ring-vinci text-lg"
             onChange={(e) => {
               setEmail(e.target.value);
             }}
@@ -44,7 +44,7 @@ const Login: FC = () => {
           {/* <input id="email-address" name="email" type="email" className="relative block w-full appearance-none rounded-none rounded-t-md border border-stone-300 px-3 py-2 text-stone-900 placeholder-stone-500 focus:z-10 focus:border-vinci focus:outline-none focus:ring-vinci sm:text-sm" placeholder="Email address"></input> */}
           <input
             type="password"
-            className="mt-4 text-black relative rounded-lg border border-stone-300 px-5 py-4 text-stone-900 placeholder-stone-400 focus:z-10 focus:border-vinci focus:outline-none focus:ring-vinci text-lg"
+            className="mt-4 text-black relative rounded-lg border border-stone-300 px-5 pt-3 pb-2 text-stone-900 placeholder-stone-300 focus:z-10 focus:border-vinci focus:outline-none focus:ring-vinci text-lg"
             onChange={(e) => {
               setPassword(e.target.value);
             }}
@@ -53,7 +53,7 @@ const Login: FC = () => {
           />
           <div className="flex items-center justify-between mt-4">
           <div className="flex items-center">
-            <input id="remember-me" name="remember-me" type="checkbox" className="h-3 w-3 rounded-lg py-4 border-stone-300 text-vinci focus:ring-vinci-light" />
+            <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 rounded-lg py-4 border-stone-300 text-vinci focus:ring-vinci-light" />
             <label htmlFor="remember-me" className="ml-1 block text-sm">Remember me</label>
           </div>
 
@@ -61,7 +61,7 @@ const Login: FC = () => {
             <a href="#" className="text-sm hover:text-indigo-500">Forgot password?</a>
           </div>
         </div>
-          <input className="font-oakesMedium mt-6 hover:bg-white/20 cursor-pointer rounded-lg bg-vinci hover:bg-vinci-hover text-lg py-4 text-white font-semibold" type="submit" value="Continue"/>
+          <input className="font-oakesMedium mt-6 hover:bg-white/20 cursor-pointer rounded-lg bg-vinci hover:bg-vinci-hover text-lg pt-3 pb-2 text-white font-semibold" type="submit" value="Continue"/>
         </form>
         <div className="h-px bg-stone-300 mt-8"> </div>
         <div className="flex items-center justify-center mt-3">
